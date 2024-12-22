97e0972b6a4fdd64c0849fae8a5905d2ad9066e6
C-W-Evans <164054958+C-W-Evans@users.noreply.github.com>
2024-12-22 11:06:52 +0100

    test 2 feature_recevoir_degats au rouge

--- app/test.py
+++ app/test.py
@@ -8,7 +8,7 @@ class Tests(unittest.TestCase):
     def test_hp_equals_10(self):
         personnage = Personnage()
         self.assertEqual(personnage.hp(), 10)
-        
+
 
     # # Tests pour feature recevoir_degats
 
@@ -17,6 +17,13 @@ class Tests(unittest.TestCase):
         defenseur = Personnage()
         defenseur.recevoir_degats(attaquant)
         self.assertEqual(defenseur.hp(), 9)
+
+    def test_attaquant_enleve_2hp(self):
+        attaquant = Personnage()
+        defenseur = Personnage()
+        defenseur.recevoir_degats(attaquant)
+        defenseur.recevoir_degats(attaquant)
+        self.assertEqual(defenseur.hp(), 8)
 
 if __name__ == "__main__":
     unittest.main()
