f58894e6c5821212eb484c593861cc068e41c2bc
C-W-Evans <164054958+C-W-Evans@users.noreply.github.com>
2024-12-22 10:34:53 +0100

    commit_initial

--- app/main.py
+++ app/main.py
@@ -1,16 +1,1 @@
 class Personnage:
-    def __init__(self):
-        self.__hp = 10
-
-    def hp(self):
-        return self.__hp
-
-    def recevoir_degats(self, attaquantS):
-        self.__hp -= 1
-
-    def est_mort(self):
-        return self.__hp <= 0
-
-    # def recevoir_soins(self, soins):
-    #     self.__hp = min(self.__hp + soins, 10)
-
